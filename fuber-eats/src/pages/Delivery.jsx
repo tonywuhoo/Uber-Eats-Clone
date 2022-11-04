08444184833a8859c62f42f37863be90130eaa43
chelseadgls <chelseamdouglas@gmail.com>
2022-11-04 16:19:35 -0400

    revisions

--- fuber-eats/src/pages/Delivery.jsx
+++ fuber-eats/src/pages/Delivery.jsx
@@ -3,13 +3,17 @@ import React from 'react'
 export default function Delivery() {
   const [menu, setMenu] = useState([]);
 
-  const getMenu = async () => {
-    const response = await fetch("https://fubereats-backend-production.up.railway.app/products")
-    const json = await response.json()
-      setMenu(json.menu)
-  }
-
-  useEffect(() => { getMenu() }, [])
+  useEffect(() => {
+    fetch('https://fubereats-backend-production.up.railway.app/products')
+       .then((response) => response.json())
+       .then((data) => {
+          console.log(data);
+          setMenu(data);
+       })
+       .catch((err) => {
+          console.log(err.message);
+       });
+ }, []);
 
   return (
     <div className='delivery'>Delivery</div>
