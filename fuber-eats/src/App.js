70c1387ba93756de965b4b221aa41694603eb732
Khristopher Patrick <khristopherpatrick@KhristophersMBP.lan>
2022-11-03 20:49:31 -0400

    addition of background image

---
+++ fuber-eats/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <nav className="navbar">
+      <div class="hamburger">
+      <span class="bar"></span>
+      <span class="bar"></span>
+      <span class="bar"></span>
+  </div>
+        FüberEats
+      </nav>
+
+
+    </>
+  );
+}
+
+export default App;
