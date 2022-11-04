4834c65ab1e51d3166595db845e76ed69862f741
mohamed khan <mohamedkhan@mohameds-MacBook-Pro.local>
2022-11-04 14:06:21 -0400

    refactor

--- fuber-eats/src/App.js
+++ fuber-eats/src/App.js
@@ -1,24 +1,18 @@
-import Menu from "./Menu.jsx"
-import Delivery from './Delivery.jsx';
-import Cart from "./Cart.jsx";
-import './App.css';
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Delivery from "./pages/Delivery";
 
-export default function Home() {
+function App() {
   return (
-    <>
-      <div className='App' style={{
-        backgroundImage: "url(/fast-food.png)"
-        }}>
-        <header>
-          <Menu className="menu"/>
-        <div className='title'>Füber Eats</div>    
- < Cart className='cart'/>
-        </header>
-        
-        <Delivery />
-      </div>
-    </>
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/delivery" element={<Delivery />} />
+      </Routes>
+    </div>
   );
 }
 
-
+export default App;
