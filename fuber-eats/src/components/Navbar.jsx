312eeeac3a90af15fd047ef6365143ed9ee29285
Khristopher Patrick <khristopherpatrick@KhristophersMBP.lan>
2022-11-05 12:07:51 -0400

    ihomepage styling

--- fuber-eats/src/components/Navbar.jsx
+++ fuber-eats/src/components/Navbar.jsx
@@ -5,15 +5,16 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <div className='navbar'>
+      
       <div className='hamburger'>
         <span className="bar"></span>
         <span className="bar"></span>
-        <span className="bar"></span>
-      </div>
+          <span className="bar"></span>
+          </div>
       <div className="logo">Fübar Eats</div>
-      <button className='Login'>Login</button>
-      <Link className='register' to="/register">Register</Link>
-      < FaShoppingCart className='cart'/>
-    </div>
+      <button className='login'>Login</button>
+      <Link to="/register">Register</Link>
+        < FaShoppingCart className='cart' />
+        </div>
   )
 }
