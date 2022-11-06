6a588ac4d6bd03828b9fd113b08694d6c2866c7f
chelseadgls <chelseamdouglas@gmail.com>
2022-11-06 11:44:11 -0500

    initial commit

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -1,7 +1,36 @@
 import React from 'react'
 import "./products.css"
+import { useState, useEffect } from 'react'
+
 export default function Products() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://fubereats-backend-production.up.railway.app/products')
+       .then((response) => response.json())
+       .then((data) => {
+          console.log(data);
+          setProducts(data);
+       })
+       .catch((err) => {
+          console.log(err.message);
+       });
+ }, []);
+
   return (
-    <div className='Products'>Products</div>
+    <div className='products'>
+      {
+      products.map((product, index) => {
+        return (
+          <div>
+          <h2>{product.name}</h2>
+          <img src={product.img} />
+          <h3>{product.price}</h3>
+          <button>Add to Cart</button>
+          </div>
+        )
+      })
+    }
+    </div>
   )
 }
