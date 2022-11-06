d5c917f45d544a3be072dbede83486bb553c3955
Tony Wu <tonywu@Tonys-Air-2.home>
2022-11-05 22:09:13 -0400

    fixed routes, added props for address,  cleaned up code structure

--- fuber-eats/src/App.js
+++ fuber-eats/src/App.js
@@ -1,27 +1,21 @@
 import './App.css';
-import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
-// import Menu from "./components/Menu";
+import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
 import Delivery from "./pages/Delivery";
+import LoginRegister from "./pages/LoginRegister"
+import ShoppingCart from "./pages/ShoppingCart";
+import { useState } from "react"
+
 
 function App() {
+  const [address, setAddress] = useState("")
+
   return (
     <div className="App">
       <Navbar />
-      {/* <Home /> */}
       <Routes>
-        <Route
-          path="/"
-          element={
-            <Home
-              style={{
-                backgroundImage:
-                  "url(https://files.slack.com/files-pri/T0351JZQ0-F04900ZRTSB/fast-food.png)",
-              }}
-            />
-          }
-        />
+        <Route path="/" element={ <Home setAddress={setAddress} />}/>
         <Route path="/delivery" element={<Delivery />} />
       </Routes>
     </div>
--- fuber-eats/src/pages/Home.jsx
+++ fuber-eats/src/pages/Home.jsx
@@ -3,16 +3,22 @@ import { Link } from "react-router-dom"
 
 
 
-export default function Home() {
+export default function Home(props) {
+
+  const handleChange = event => {
+    props.setAddress(event.target.value)
+
+  }
+  const submitAddress = event => {
+    console.log("Clicked")
+  }
   return (
     <div className='home'style={{backgroundImage: "url(/fast-food.png)"}}>
-      <div className='hungry'>Get Food Now!</div>
-      <div className="address">
-      <input className='input' type="text"
-        name="Address"
-      placeholder='Enter address here'/>
-      <button><Link to="/delivery">Find Food</Link></button>
-      </div>
+    <div className='hungry'>Get Food Now!</div>
+    <div className="address">
+    <input className='input' type="text" onChange= { handleChange } name="Address" placeholder='Enter address here'/>
+    <button onClick = { submitAddress }><Link to="/Cart">Delivery</Link></button>
+    </div>
     </div>
   )
 }
