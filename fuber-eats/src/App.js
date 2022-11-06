58db6215b145645d014c7b809f284c8b7f3a90e1
mohamed khan <mohamedkhan@mohameds-MacBook-Pro.local>
2022-11-06 03:29:45 -0500

    move Login/Register state from app.js -> LoginRegister.jsx

--- fuber-eats/src/App.js
+++ fuber-eats/src/App.js
@@ -1,60 +1,24 @@
-import './App.css';
+import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
 import Delivery from "./pages/Delivery";
-import LoginRegister from "./pages/LoginRegister"
-import About from './pages/About';
-import Products from "./pages/Products"
+import LoginRegister from "./pages/LoginRegister";
+import About from "./pages/About";
+import Products from "./pages/Products";
 import Cart from "./pages/Cart";
-import { useState } from "react"
-
 
 function App() {
-  const [address, setAddress] = useState("")
-  const [LoginStatus, setLoginStatus] = useState(false)
-  const [RegisterUser, setRegisterUser] = useState("")
-  const [RegisterPassword, setRegisterPassword] = useState("")
-  const [RegisterConfirmPassword, setRegisterConfirmPassword] = useState("")
-  const [LoginUser, setLoginUser] = useState("")
-  const [LoginPassword, setLoginPassword] = useState("")
-  const [LoginConfirmPassword, setLoginConfirmPassword] = useState("")
-  const [Encrypted, setEncrypted] = useState("")
-  const [userHash, setuserHash] = useState("")
   return (
     <div className="App">
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home
-          setAddress={setAddress}
-          address = {address}
-        />} />
+        <Route path="/" element={<Home />} />
         <Route path="/delivery" element={<Delivery />} />
         <Route path="/About" element={<About />} />
         <Route path="/Products" element={<Products />} />
         <Route path="/Cart" element={<Cart />} />
-        <Route path="/LoginRegister" element={<LoginRegister
-          RegisterUser={RegisterUser}
-          setRegisterUser={setRegisterUser}
-          RegisterPassword={RegisterPassword}
-          setRegisterPassword={setRegisterPassword}
-          RegisterConfirmPassword={RegisterConfirmPassword}
-          setRegisterConfirmPassword={setRegisterConfirmPassword}
-
-          LoginUser = {LoginUser}
-          setLoginUser = {setLoginUser}
-          LoginPassword = {LoginPassword}
-          setLoginPassword = {setLoginPassword}
-          LoginConfirmPassword = {LoginConfirmPassword}
-          setLoginConfirmPassword = {setLoginConfirmPassword}
-          
-          LoginStatus={LoginStatus}
-          setLoginStatus={setLoginStatus}
-          Encrypted={Encrypted}
-          setEncrypted={setEncrypted}
-          userHash={userHash}
-          setuserHash = {setuserHash}
-        />} />
+        <Route path="/LoginRegister" element={<LoginRegister />} />
       </Routes>
     </div>
   );
