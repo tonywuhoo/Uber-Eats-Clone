6b9289e3e19f215aa078075093a86cc77cacd8a6
chelseadgls <chelseamdouglas@gmail.com>
2022-11-08 16:01:22 -0500

    updated detail to card

--- src/pages/productdetail/ProductDetail.jsx
+++ src/pages/productdetail/ProductDetail.jsx
@@ -3,6 +3,8 @@ import { useState, useEffect } from 'react';
 import { Link, useParams, useNavigate } from 'react-router-dom';
 import { getProduct } from "../../services/products";
 import "./ProductDetail.css";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 function ProductDetail() {
   const [product, setProduct] = useState([])
@@ -26,16 +28,24 @@ function ProductDetail() {
     <div>
     <Link to={`/Products/`}>
       <div className="back">Back to Products</div>
-    </Link>
-    <div className="product-detail">
-      <img className="product-detail-img" src={product.img} alt={product.name} />
-      <div className="detail">
-          <div className="name"><h1>{product.name}</h1></div>
-          <div className="description"><h3>{product.dsc}</h3></div>
-          <div className="price"><h3>{product.price}</h3></div>
-          <div className="cart"><button>Add to Cart</button></div>
-      </div>
-      </div>
+      </Link>
+      <div className="detail-container">
+    <Card style={{ width: '30rem' }}>
+      <Card.Img variant="top" src={product.img} />
+      <Card.Body>
+      <Link to={`/Products/${product._id}`}>
+        <Card.Title>{product.name}</Card.Title>
+        <Card.Text>
+          {product.dsc}
+        </Card.Text>
+        <Card.Text>
+          ${product.price}
+         </Card.Text> 
+         </Link>
+        <Button variant="primary" onClick={() => addToCart(product)}>Add to Cart</Button>
+      </Card.Body>
+        </Card>
+        </div>
       </div>
   )
 }
