e98cbca0b7fe42bd1b0e9d07102486333220f01b
chelseadgls <chelseamdouglas@gmail.com>
2022-11-08 08:43:24 -0500

    added product detail

--- src/services/products.js
+++ src/services/products.js
@@ -12,6 +12,18 @@ export const getProducts = async () => {
   }
 };
 
+export const getProduct = async (id) => {
+  try {
+    const response = await axios.get(
+      `https://fubereats-backend-production.up.railway.app/products/${id}`
+    );
+    // console.log(response.data);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
 export const getBBQ = async () => {
   try {
     const response = await axios.get(
