43c4fedf3b53a3fcf874fa16ab58e0e401764a6e
mohamed khan <mohamedkhan@mohameds-MacBook-Pro.local>
2022-11-07 23:58:56 -0500

    installed bootstrap with npm, added script to publix/index.html, created bootstrap navbar, updated home page, moved landing image and text to hero component, added another section to homepage, created cardGroup component and imported to homepage

---
+++ src/components/cardGroup/cardGroup.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import "./cardGroup.css"
+
+export default function cardGroup() {
+  return (
+      <div class="card-group">
+        <div class="card">
+          <img
+            class="card-img-top"
+            src="https://picsum.photos/800"
+            alt="Card image cap"
+          />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              This is a wider card with supporting 
+            </p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </div>
+        </div>
+        <div class="card">
+          <img
+            class="card-img-top"
+            src="https://picsum.photos/800"
+            alt="Card image cap"
+          />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              This card has supporting text
+            </p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </div>
+        </div>
+        <div class="card">
+          <img
+            class="card-img-top"
+            src="https://picsum.photos/800"
+            alt="Card image cap"
+          />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              This is a wider card with
+            </p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </div>
+        </div>
+      </div>
+  )
+}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,34 +1,16 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import CardGroup from "../../components/cardGroup/cardGroup.jsx"
+import Hero from "../../components/hero/Hero.jsx"
 import "./home.css";
 
 export default function Home(props) {
-  const handleChange = (event) => {
-    props.setAddress(event.target.value);
-  };
-  const submitAddress = (event) => {
-    console.log("Clicked");
-  };
   return (
     <div className="home">
-      <div
-        className="skillet"
-        style={{ backgroundImage: "url(/fast-food.png)" }}
-      >
-        <div className="hungry">Get Food Now!</div>
-        <div className="address">
-          <input
-            className="input"
-            type="text"
-            onChange={handleChange}
-            name="Address"
-            placeholder="Enter address here"
-          />
-          <button className="food" onClick={submitAddress}>
-            <Link to="/Products">Delivery</Link>
-          </button>
-        </div>
-      </div>
+      <Hero />
+      <section className="home-sect2">
+        <h2>Lorem ipsum dolor sit.</h2>
+        <CardGroup />
+      </section>
     </div>
   );
 }
