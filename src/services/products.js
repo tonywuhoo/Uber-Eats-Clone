deff5419e5b6881d4401127ec6dfdbb733460c8c
chelseadgls <chelseamdouglas@gmail.com>
2022-11-09 09:52:12 -0500

    added new pages

--- src/services/products.js
+++ src/services/products.js
@@ -36,60 +36,50 @@ export const getBBQ = async () => {
   }
 };
 
-// export const getLimitedProducts = async () => {
-//   try {
-//     const response = await axios.get(
-//       "https://fubereats-backend-production.up.railway.app/products"
-//     );
-//     // console.log(response.data);
-//     return response.data;
-//   } catch (error) {
-//     throw error;
-//   }
-// };
+export const getDesserts = async () => {
+  try {
+    const response = await axios.get(
+      "https://ig-food-menus.herokuapp.com/desserts"
+    );
+    // console.log(response.data);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
 
-// export const getLeague = async (id) => {
-//   try {
-//     const response = await axios.get(
-//       `https://soccer-standings-api-production.up.railway.app/leagues/${id}`
-//     );
-//     return response.data;
-//   } catch (error) {
-//     throw error;
-//   }
-// };
+export const getPizza = async () => {
+  try {
+    const response = await axios.get(
+      "https://ig-food-menus.herokuapp.com/pizzas"
+    );
+    // console.log(response.data);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
 
-// export const createLeague = async (leagueData) => {
-//   try {
-//     const response = await axios.post(
-//       "https://soccer-standings-api-production.up.railway.app/leagues",
-//       leagueData
-//     );
-//     return response.data;
-//   } catch (error) {
-//     throw error;
-//   }
-// };
+export const getDrinks = async () => {
+  try {
+    const response = await axios.get(
+      "https://ig-food-menus.herokuapp.com/drinks"
+    );
+    // console.log(response.data);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
 
-// export const updateLeague = async (id, leagueData) => {
-//   try {
-//     const response = await axios.put(
-//       `https://soccer-standings-api-production.up.railway.app/leagues/${id}`,
-//       leagueData
-//     );
-//     return response.data;
-//   } catch (error) {
-//     throw error;
-//   }
-// };
-
-// export const deleteLeague = async (id) => {
-//   try {
-//     const response = await axios.delete(
-//       `https://soccer-standings-api-production.up.railway.app/leagues/${id}`
-//     );
-//     return response.data;
-//   } catch (error) {
-//     throw error;
-//   }
-// };
+export const getSandwiches = async () => {
+  try {
+    const response = await axios.get(
+      "https://ig-food-menus.herokuapp.com/sandwiches"
+    );
+    // console.log(response.data);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
